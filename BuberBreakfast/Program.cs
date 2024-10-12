662321f58918e4bac726a8b78faada19be156faf
carlosxmerca <eduarmercado4@gmail.com>
2024-10-11 23:49:23 -0600

    :tada: add: BuberBreakfast API project

---
+++ BuberBreakfast/Program.cs
@@ -0,0 +1,27 @@
+using BuberBreakfast.Services.Breakfasts;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
+    builder.Services.AddScoped<IBreakfastService, BreakfastService>();
+}
+
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+var app = builder.Build();
+{
+    app.UseExceptionHandler("/error");
+    app.UseHttpsRedirection();
+    // app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
